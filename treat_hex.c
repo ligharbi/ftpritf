1427f883a88a0034499a1d6011982a41d6c652f2
ligharbi <ligharbi@student.42roma.it>
2025-01-11 20:22:07 +0100

    Add files via upload

--- treat_hex.c
+++ treat_hex.c
@@ -1,35 +1,38 @@
-char	*m_rev(char *s, char c)
-{
-	char	*ret;
-	size_t	len;
-
-	len = ft_len(s);
-	ret = malloc (len + 1);
-	if (ret == NULL)
-		return (NULL);
-	if (c == 'X')
-		s = ft_toupper(s);
-	while (len > 0)
-	{
-		*ret = s[len];
-		len --;
-		ret ++;
-	}
-	*ret = '\0';
-	return ret;
-}
 char	*treat_hex(int n, char c)
 {
+	int	len;
 	char	*s;
+	
+	len = length_hexa(n);
+	s = malloc (len + 1);
+	if (s == NULL)
+		return NULL;
+	s[len] = '\0';
+	if (n == 0)
+		s[0] = '0';
 	while (n > 0)
 	{
+		len --;
 		if (n % 16 >= 10)
-			*s = ('a' + (n % 16 - 10));
+			s[len] = ('a' + (n % 16 - 10));
 		else
-			*s = '0' + (n % 16);
+			s[len] = '0' + (n % 16);
 		n = n / 16;
-		s ++;
 	}
-	*s = '\0';
-	return (m_rev(s, c));
+	if (c == 'X')
+		return (ft_toupper(s));
+	return (s);
+}
+int	length_hexa(int n)
+{
+	int	i;
+	i = 0;
+	if (n == 0)
+		i ++;
+	while (n > 0)
+	{
+		i += 1;
+		n = n / 16;
+	}
+	return i;
 }
