8b402b6a5a3bd94d984abd4d2679afb814d2df91
ligharbi <ligharbi@student.42roma.it>
2025-01-12 23:43:42 +0100

    need to add flags

--- ft_printf.c
+++ ft_printf.c
@@ -1,17 +1,38 @@
 int	ft_printf (const char *s, ...)
 {
+	char	*str;
+	int	ret_total_inputs;
 	va_list	arg;
-	va_start(args, s);
-	if(flags)
-		treat_flags(s);
-	while(get_specifier(*s) != '\0')
-	{
-		va_arg(arg,get_specifier(*s);
-		r = concat(r,treat_arg(va_arg));
-		s++;
 
+	va_start(arg, s);
+	ret_total_inputs = 0;
+	while (*s != '\0')
+	{
+		if(*s == '%')
+		{
+			while (is_ident(s) == 1)
+			{
+				if (*(s+1) == 'd' || *(s+1) == 'i')
+					str= treat_d(va_arg(arg, int));
+				if (*(s +1) == 'c')
+					str = treat_c(va_arg(arg, char));
+				if (*(s + 1) == '%' && *s == '%')
+					str = treat_c('%');
+				if (*(s + 1) == 'p')
+                                        str = treat_p(va_arg(arg, unsigned long long));
+				if (*(s + 1) == 'x' || *(s + 1) == 'X')
+					str = treat_hexa(va_arg(arg, unsigned int));
+				ret_total_inputs += ft_putstr(str);
+				free(str);
+				s++;
+			}
+		}
+		ret_total_inputs ++;
+		ft_putchar(*s);
+		s ++;
 	}
-	print_str(r);
+	va_end(arg);
+	return (ret_total_inputs);
 }
 
 
